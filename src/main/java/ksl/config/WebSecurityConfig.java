1835aa4b38617ac4730cf5d1b1f356e10fa0fff5
protik.dey <protikdey490@gmail.com>
2023-12-17 14:01:52 +0600

    spring updated to 3.2 and java 21

--- src/main/java/ksl/config/WebSecurityConfig.java
+++ src/main/java/ksl/config/WebSecurityConfig.java
@@ -24,8 +24,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .cors().disable()
+//                .csrf().disable()
+//                .cors().disable()
 //                .authorizeHttpRequests()
 //                .requestMatchers("/**").permitAll()
 //                .and()
